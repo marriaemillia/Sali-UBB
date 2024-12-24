b574f22d195beb57c61d50c9ed10888c64bd4b78
Maria Emilia Vizitiu Pietrareanu <120202739+marriaemillia@users.noreply.github.com>
2024-12-24 11:28:32 +0200

    Create calendar.cod.database.sql

---
+++ calendar.cod.database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE calendar;
+
+USE calendar;
+
+CREATE TABLE events (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date DATE NOT NULL,
+  time TIME NOT NULL,
+  description VARCHAR(255) NOT NULL
+);
